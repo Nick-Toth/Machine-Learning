c77818686ca7dedb94660aed8d248bb50321afb8
Nick Thomas <ntoth@pdx.edu>
2017-11-17 00:37:45 -0800

    Update perceptron for testing with mnist data

--- Haskell-Perceptron/PerceptronTest.hs
+++ Haskell-Perceptron/PerceptronTest.hs
@@ -11,44 +11,23 @@
 //
 \\ ************************************************ -}
 
-
 import Perceptron
-import TrainingData
+import TrainingData -- Training data generation
 
 
--- The number of packets in each bundle/epoch.
-packets = 10 :: Int
-
+-- The number of packets in each training data bundle.
+packets = 150 :: Int
 -- For the random data generator.
 seed = 0 :: Int
 
 
 main :: IO ()
 main = do
+  test_gen -- Simulate perceptron with binary input data.
+  --test_mnist -- Simulate perceptron with mnist data.
+  putStrLn "\n\n  Goodbye!\n"
 
-  -- Simulate the perceptron learning algorithm
-  output <- test_gen
-
-  -- Display the results of training the network.
-  printMeta output
-
-  {- Test final weights -}
-  let complete_dataset = [[0,0], [0,1], [1,0], [1,1]]
-      -- Extract final weights from output
-      fin_wts = fst output
-      -- Perceptron output function. ∑ Weights•Inputs - bias
-      test [a, b] = (foldl (+) 0 (zipWith (*) [a, b] fin_wts)) - threshold
-      -- Execute output function on testing set.
-      -- If all goes well, tst_out = [0,0,1,1].
-      tst_out = map ((\y -> if y >= 0 then 1 else 0) . test) complete_dataset
-
-  putStrLn $ "\n\n  Post-Training test:\n"
-          ++ "\n    Complete input set: " ++ show complete_dataset
-          ++ "\n    Correct outputs:    " ++ "[0,0,1,1]"
-          ++ "\n    Test Outputs:       " ++ show tst_out
-          ++ "\n\n Goodbye!\n"
-
-
+--(foldl (+) 0 (zipWith (*) [0.0,1.0] [3.2759907e-3,2.4698852e-3])) - threshold
 {- Simulate perceptron on random binary data
    created with the generator in TrainingData.hs.
 
@@ -58,43 +37,101 @@ main = do
       (1, 0) => 1
       (1, 1) => 1
 -}
-test_gen :: IO ([Float], Float)
+test_gen :: IO ()
 test_gen = do
-
   -- Initialize a list of randomized weights.
   weights <- genWeights 2
 
+  let packs = min packets 10
       -- Generate random training data.
-  let raw_data = genDataBundle packets seed
+      raw_data = genDataBundle packs seed
       -- Convert data format from [((Float, Float), Float)] to [([Float], Float)]
-      bundle = cnv_b raw_data
+      -- Use explicit bundle for guaranteed correct output.
+      bundle = cnv_b raw_data -- bundle = [([0,0], 0), ([0,1],0), ([1,0],1), ([1,1],1)]
       -- Train the network
       output = train bundle weights
+      -- Extract data from output.
+      fnl_wts = fst output
+      epochs = snd output
+      -- Perceptron output function. ∑ Weights•Inputs - bias
+      pcep_test tst_inp = (foldl (+) 0 (zipWith (*) tst_inp fnl_wts)) - threshold
+      test_set = [[0,0], [0,1], [1,0], [1,1]]
+      -- Execute output function on testing set.
+      tst_out = map ((\y -> if y >= 0 then 1 else 0) . pcep_test) test_set
 
   -- Display the test data.
-  printData raw_data
+  printBinData raw_data
 
-  return output
+  -- Print general metadata about the perceptron's training
+  putStrLn $ "\n  Perceptron trained with:\n"
+          ++ "\n    Training-data packets: " ++ show packs
+          ++ "\n    Epochs:                " ++ show epochs
+          ++ "\n    Final Weights:         " ++ show fnl_wts
+          -- ++ "\n    Final Weights: " ++ show weights
+          ++ "\n    Learning rate:         " ++ show l_rate
+          ++ "\n    Threshold:             " ++ show threshold
+          -- Separate metadata and test data.
+          ++ "\n\n    " ++ take 35 (cycle "-")
+          ++ "\n\n  Post-Training test:\n"
+          ++ "\n    Complete input set: " ++ show test_set
+          ++ "\n    Correct outputs:    " ++ "[0,0,1,1]"
+          ++ "\n    Perceptron outputs: " ++ show tst_out
 
 
--- Convert a string of binary digits into a list of bits (Float).
-bitVec :: String -> [Float]
-bitVec bitmap = map (\wrd -> read [wrd] :: Float) bitmap
+{- Simulate perceptron algorithm for classifying
+   hand written digits from the mnist data set.
+   
+   Creates a list of 10 perceptrons (each corresponding to
+   a hand written digit), and trains them to classify the
+   mnist data set.
+-}
+test_mnist :: IO ()
+test_mnist = do
 
--- Convert a list of strings of binary digits into a list of lists bits (Float).
-bitVec_fromMtx :: [String] -> [[Float]]
-bitVec_fromMtx bitmap = map (\x -> map ( \y -> (read y :: Float) ) (words x)) ( bitmap)
+  -- Initialize a list of randomized weights.
+  weights <- genWeights 784
+
+  -- mnist training data.
+  trn_data <- scanMNIST packets
+  tst_data <- scanMNIST 1
+
+      -- Remove newlines, and convert training data strings to floats.
+  let bitmap_strs = map (\img -> bitVec (filter (/= '\n') img)) (map fst trn_data)
+      -- Convert labels from string to float.
+      labels = map (\lab -> read lab :: Float) (map snd trn_data)
+      -- Bundle training data and labels.
+      bundle = zip bitmap_strs labels
+      -- Train each perceptron. The digit specified by x corresponds
+      -- to the digit that the perceptron will learn to classify.
+      output = map (\x -> train_cl bundle weights x) [0..9]
+      -- De-list testing data.
+      (inp_str, lab_str) = tst_data !! 0
+      -- Convert input data to list of Floats.
+      tst_inp = bitVec (filter (/= '\n') inp_str)
+      tst_lab = read lab_str :: Float
+      -- Extract final weights for the perceptron
+      -- corresponding to the selected test data.
+      fnl_wts = fst (output !! round tst_lab)
+      -- Calculate the perceptron's output.
+      pcep_test = binCond (>= 0) (foldl (+) 0 (zipWith (*) tst_inp fnl_wts) - threshold)
+
+  -- Print general metadata about training
+  putStrLn $ "\n  Perceptrons (10) trained with: \n"
+          ++ "\n    Training-data packets: " ++ show packets
+          ++ "\n    Epochs:                " ++ show (map snd output)
+          ++ "\n    Learning rate:         " ++ show l_rate
+          ++ "\n    Threshold:             " ++ show threshold
+          ++ "\n\n  " ++ take 35 (cycle "-")
+          ++ "\n\n  Post-Training test:\n"
+          ++ "\n    Test data label: " ++ show tst_lab
+          ++ "\n    Correct outputs: " ++ show (outputVec tst_lab)
+          ++ "\n    Test outputs:    " ++ show (outputVec pcep_test)
 
 
--- Print metadata for perceptron training algorithm results.
-printMeta :: ([Float], Float) -> IO ()
-printMeta output = putStrLn $
-     "\n  Perceptron trained with a dataset of size " ++ show packets ++ "\n"
-  ++ "\n    Epochs:        " ++ show epochs
-  ++ "\n    Final Weights: " ++ show weights
-  ++ "\n    Learning rate: " ++ show l_rate
-  ++ "\n    Threshold:     " ++ show threshold
-    where
-      -- Extract data from output.
-      weights = fst output
-      epochs = snd output
+-- A conditional function which returns 1 for true, 0 for false.
+binCond :: Num p => (t -> Bool) -> t -> p
+binCond cnd x = if cnd x then 1 else 0
+
+-- Creates a list of 10 bits. The bit at index valid is 1, others are all 0.
+outputVec :: (Eq a, Num b, Num a, Enum a) => a -> [b]
+outputVec valid = map (binCond (== valid)) [0..9]
