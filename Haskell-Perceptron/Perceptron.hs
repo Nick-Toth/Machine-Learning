053f15c45f7c5c5fc11efa5cde1135972b705f06
Nick Thomas <ntoth@pdx.edu>
2017-11-17 00:36:33 -0800

    Update perceptron to train on mnist data.
    train_cl function added for training multiple perceptrons in parallel. See comments for more info.

--- Haskell-Perceptron/Perceptron.hs
+++ Haskell-Perceptron/Perceptron.hs
@@ -4,16 +4,18 @@
 \\ Email: ntoth@pdx.edu
 // Date:  November 12, 2017
 \\
-// Overview: Example of a perceptron in haskell.
+// Overview: A typical perceptron implementation.
 \\
 // ************************************************ -}
 
 
 module Perceptron ( genWeights,
                     train,
+                    train_cl,
                     l_rate,
                     threshold,
                     sgn ) where
+
 
 import System.Random (getStdRandom, randomR)
 import Control.Monad (replicateM)
@@ -26,8 +28,17 @@ type DataPacket = (Inputs, Target); type DataSet = [DataPacket]
 
 
 -- Default network parameters.
-l_rate = 0.1 :: Float
-threshold = 0.2 :: Float
+l_rate = 0.002 :: Float
+threshold = 0.002 :: Float
+
+
+{- Target transformation for training multiple perceptrons in parallel.
+   For example, in classifying the minst dataset, this function maps
+   each target to 1. This is done to maintain binary inputs/outputs. -}
+targTrans :: Target -> Target -> Target
+targTrans target target' | target == target' = 1
+                         | otherwise = 0
+
 
 -- Create a list of randomized floats in the range (-0.5, 0.5).
 genWeights :: Int -> IO [Float]
@@ -41,7 +52,7 @@ sgn inputs weights = sgn' $ (foldl (+) 0 (zipWith (*) inputs weights)) - thresho
 
 -- Calculate the perceptron's binary output.
 sgn' :: Output -> Output
-sgn' y | y >= 0    = 1
+sgn' y | y >= 0 = 1
        | otherwise = 0
 
 
@@ -67,29 +78,40 @@ iter inputs weights target =
 
 {- Delegate of train.
    Run each packet of data through the perceptron. -}
-epoch :: DataSet -> Weights -> DataPacket
-epoch inputs weights = (updated_weights, err)
+epoch :: DataSet -> Weights -> Target -> DataPacket
+epoch inputs weights target' = (u_weights, err)
     where
       -- Iter function alias for updating weights with foldl.
       iter_f weights (inputs, target) = iter inputs weights target
       -- Train the network on each data packet.
-      updated_weights = foldl iter_f weights inputs
+      u_weights = foldl iter_f weights inputs
       -- Calculate the epoch error average.
       weight_len = fromIntegral $ length weights
-      zipped_weights = map abs (zipWith (-) updated_weights weights)
+      zipped_weights = map abs (zipWith (-) u_weights weights)
       err = foldl (+) 0 zipped_weights / weight_len
 
 
--- Initialize the first train' call.
+{- Initialize the first train' call (for binary perceptron).
+   @inputs: A list of tuples containing training data and labels.
+   @weights: A list of starting weights.
+-}
 train :: DataSet -> Weights -> DataPacket
-train inputs weights = train' inputs weights 1
+train inputs weights = train' inputs weights 1 1
+
+
+{- Initialize the first train' call (for parallel perceptrons).
+   @target': If a given perceptron is being trained to classify
+             the hand written digit 8, then target' should be 8. -}
+train_cl :: DataSet -> Weights -> Target -> DataPacket
+train_cl inputs weights target' = train' inputs weights 1 target'
+
 
 -- Simulate the perceptron learning algorithm.
-train' :: DataSet -> Weights -> Float -> DataPacket
-train' inputs weights epochs
+train' :: DataSet -> Weights -> Float -> Target -> DataPacket
+train' inputs weights epochs target'
       -- If the epoch error is zero, return the results.
-    | err == 0.0 = (updated_weights, epochs)
+    | err == 0.0 = (u_weights, epochs)
       -- If the previous epoch error was not zero, train another epoch.
-    | otherwise = train' inputs updated_weights (epochs + 1)
+    | otherwise = train' inputs u_weights (epochs + 1) target'
         -- Train the perceptron (1 epoch).
-        where (updated_weights, err) = epoch inputs weights
+        where (u_weights, err) = epoch [ (fst x, targTrans (snd x) target') | x <- inputs] weights target'
